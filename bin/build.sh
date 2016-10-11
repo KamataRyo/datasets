acd9d465357ea10820725aa35976d79c64a5d228
KamataRyo <mugil.cephalus+Pagophilla.local@gmail.com>
2016-10-11 13:37:32 +0900

    Make biuld script robust

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-# DATA_FOLDER='data'
-# if [[ ! -d "$DATA_FOLDER" ]]; then
-#     mkdir "$DATA_FOLDER"
-# fi
-# cd $DATA_FOLDER
-tar xfz ldgourmet.tar.gz
+DATA_FOLDER='data'
+if [[ ! -d "$DATA_FOLDER" ]]; then
+    mkdir "$DATA_FOLDER"
+fi
+cd $DATA_FOLDER && tar xfz ldgourmet.tar.gz
